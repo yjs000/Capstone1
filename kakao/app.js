b0fa76927861db41816cbb126a07b3f5d505575d
yjs000 <jisu91113@gmail.com>
2021-04-14 02:11:53 +0900

    commit

---
+++ kakao/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const morgan = require('morgan');
+const path = require('path');
+
+const port = 3000
+const app = express()
+
+app.use(morgan('dev'));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'circle.html'));
+// })
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'xy_to_address.html'));
+})
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`)
+})
